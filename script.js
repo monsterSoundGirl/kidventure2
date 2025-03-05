3dd9b0573aa214d2b49f38137199d56aa7119704
Christian Wright <163559084+monsterSoundGirl@users.noreply.github.com>
2025-03-05 14:42:11 -0600

    next

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
 // script.js
 
-// This function will run when Miro's SDK has loaded
 window.onload = async () => {
-  // Optional: Wait for Miro to be 'ready' if you need the SDK
+  // The new Miro Web SDK still uses 'miro.onReady' in v2:
   await miro.onReady(() => {
-    console.log('Kidventure TTRPG App is loaded in Miro!');
+    console.log('Kidventure app is loaded inside Miro!');
+
+    // Create a button in your webpage
+    const button = document.createElement('button');
+    button.innerText = 'Create Sticky Note';
+    button.onclick = async () => {
+      await miro.board.createStickyNote({
+        content: 'Hello from Kidventure v2!',
+        x: 0,
+        y: 0,
+      });
+    };
+    document.body.appendChild(button);
   });
-
-  // Simple example: create a button in your page
-  const button = document.createElement('button');
-  button.innerText = 'Create a Sticky Note';
-  button.onclick = createStickyNote;
-  document.body.appendChild(button);
 };
-
-async function createStickyNote() {
-  // When pressed, create a sticky note at coordinates (0, 0)
-  // inside the current Miro board.
-  try {
-    const sticky = await miro.board.widgets.create({
-      type: 'sticky_note',
-      x: 0,
-      y: 0,
-      text: 'Hello from Kidventure!',
-      style: {
-        stickerBackgroundColor: '#F9DF74'
-      }
-    });
-    console.log('Created sticky note:', sticky);
-  } catch (err) {
-    console.error('Could not create sticky note', err);
-  }
-}
